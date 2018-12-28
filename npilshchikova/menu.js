4d01b26905a31ee1bb20c308e3a74807c005fe66
Nadezhda Pilshchikova <npilshchikova@parseq.pro>
2018-12-28 18:23:43 +0300

    part of menu methods with strange type error

--- npilshchikova/menu.js
+++ npilshchikova/menu.js
@@ -3,18 +3,101 @@
  */
 module.exports = {
 
-    items = [],
+    items: [],
 
+    /**
+     * Add new Humburger with stuffing and size specified to menu
+     * 
+     * @param {String} size 
+     * @param {String} stuffing 
+     */
     addHamburger(size, stuffing) {
-        // new Hamburger to menu
+        var newStuffing;
+        var newHamburger;
+
+        // select stuffing
+        switch (stuffing) {
+            case Stuffing.CHEESE:
+                newStuffing = new Stuffing(stuffing, price=10, calories=20);
+                break;
+            case Stuffing.POTATO:
+                newStuffing = new Stuffing(stuffing, price=15, calories=10);
+                break;
+            default:
+                newStuffing = new Stuffing(stuffing, price=20, calories=5);
+        };
+
+        // select size
+        switch (size) {
+            case Hamburger.SMALL:
+                newHamburger = new Hamburger(size, stuffing=newStuffing, price=50, calories=20);
+                break;
+            default:
+                newHamburger = new Hamburger(size, stuffing=Stuffing, price=100, calories=40);
+        }
+
+        // add Hamburger to menu
+        this.items.push(newHamburger);
+
+        return this;
     },
 
+    /**
+     * Add defined amount of Salad to menu
+     * 
+     * @param {String} name 
+     * @param {Number} weight 
+     */
     addSalad(name, weight) {
-        // new Salad to menu
+        
+        // if Salad already added, just increase it's amount
+        var saladFound = false;
+        for (var i = 0; i < this.items.length; i++) {
+            var nextItem = this.items[i];
+            if (nextItem.type === Meal.SALAD && nextItem.name === name) {
+                nextItem.weight += weight;
+                saladFound = true;
+            }
+        }
+
+        // otherwise, add new Salad
+        if (!saladFound) {
+            var newSalad;
+
+            switch (name) {
+                case Salad.RUSSIAN:
+                    newSalad = new Salad(name, weight, price=50, calories=80);
+                    break;
+                default:
+                    newSalad = new Salad(name, weight, price=100, calories=20);
+            }
+            this.items.push(newSalad);
+        }
+
+        return this;
     },
 
+    /**
+     * Add defined Drink to menu
+     * 
+     * @param {String} name 
+     */
     addDrink(name) {
-        // new drink to menu
+        var newDrink;
+
+        // select drink
+        switch (name) {
+            case Drink.COLA:
+                newDrink = new Drink(name, price=50, calories=40);
+                break;
+            default:
+                newDrink = new Drink(name, price=80, calories=20);
+        }
+
+        // add Hamburger to menu
+        this.items.push(newDrink);
+
+        return this;
     },
 
     removeHamburger(size, stuffing) {
@@ -29,8 +112,34 @@ module.exports = {
         // remove target drink if exists
     },
     
-    pay() {
-        // items list should became immutable
+    /**
+     * Submut current order
+     * After that, it is impossible to add or remove items from order
+     */
+    submit() {
+        Object.defineProperty(this, 'items', {writable: false});
+    },
+
+    /**
+     * Calculate price for items selected
+     */
+    calculatePrice() {
+        var price = 0;
+        for (var i = 0; i < this.items.length; i++) {
+            price += this.items[i].calculatePrice();
+        }
+        return price;
+    },
+
+    /**
+     * Calculate calories for items selected
+     */
+    calculateCalories() {
+        var calories = 0;
+        for (var i = 0; i < this.items.length; i++) {
+            calories += this.items[i].calculateCalories();
+        }
+        return calories;
     }
 
 }
@@ -49,6 +158,10 @@ function Meal(type, price, calories) {
     this.price = price;
     this.calories = calories;
 }
+Meal.SALAD = 'salad';
+Meal.HAMBURGER = 'hamburger';
+Meal.STUFFING = 'stuffing';
+Meal.DRINK = 'drink';
 
 /**
  * Calculate Meal price
@@ -75,10 +188,10 @@ Meal.prototype.calculateCalories = function() {
  * @param {Number} calories
  */
 function Stuffing(name, price, calories) {
-    Meal.call(this, 'stuffing', price, calories);
+    Meal.call(this, Meal.STUFFING, price, calories);
     this.name = name;
 }
-Stuffing.prototype = Object.create(Meal);
+Stuffing.prototype = Object.create(Meal.prototype);
 Stuffing.prototype.constructor = Stuffing;
 Stuffing.SALAD = 'salad';
 Stuffing.CHEESE = 'cheese';
@@ -94,14 +207,14 @@ Stuffing.POTATO = 'potato';
  * @param {Number} calories
  */
 function Hamburger(size, stuffing, price, calories) {
-    Meal.call(this, 'hamburger', price, calories);
+    Meal.call(this, Meal.HAMBURGER, price, calories);
     this.size = size;
     this.stuffing = stuffing;
 }
-Hamburger.prototype = Object.create(Meal);
+Hamburger.prototype = Object.create(Meal.prototype);
 Hamburger.prototype.constructor = Hamburger;
-Hamburger.SIZE_SMALL = 'small';
-Hamburger.SIZE_LARGE = 'large';
+Hamburger.SMALL = 'small';
+Hamburger.LARGE = 'large';
 
 /**
  * Calculate price of Hamburger including stuffing
@@ -129,12 +242,14 @@ Hamburger.prototype.calculateCalories = function() {
  * @param {Number} calories  Calories per 100 gramm
  */
 function Salad(name, weight, price, calories) {
-    Meal.call(this, 'salad', price, calories);
+    Meal.call(this, Meal.SALAD, price, calories);
     this.name = name;
     this.weight = weight;
 }
-Salad.prototype = Object.create(Meal);
+Salad.prototype = Object.create(Meal.prototype);
 Salad.prototype.constructor = Salad;
+Salad.RUSSIAN = 'russian salad';
+Salad.CAESAR = 'caesar';
 
 /**
  * Calculate price of Salad with portion weight taken into account
@@ -156,10 +271,10 @@ Salad.prototype.calculateCalories = function() {
  * @param calories
  */
 function Drink(name, price, calories) {
-    Meal.call(this, 'drink', price, calories);
+    Meal.call(this, Meal.DRINK, price, calories);
     this.name = name;
 }
-Drink.prototype = Object.create(Meal);
+Drink.prototype = Object.create(Meal.prototype);
 Drink.prototype.constructor = Drink;
 Drink.COLA = 'cola';
 Drink.COFFEE = 'coffee';
--- npilshchikova/tests.js
+++ npilshchikova/tests.js
@@ -2,8 +2,26 @@ function testMenu() {
     // tests module
     var assert = require('assert');
 
-    // menu object
-    var menu = require('./menu.js');
+    // order object
+    var order = require('./menu.js');
+
+    order.addHamburger('small', 'salad');
+    order.addHamburger('large', 'potato').addDrink('cola');
+
+    console.log('Your order price:', order.calculatePrice());
+    console.log('Your order caloricity:', order.calculateCalories());
+
+    order.removeHamburger('small', 'salad');
+    order.removeDrink('cola');
+
+    console.log('Your order price:', order.calculatePrice());
+    console.log('Your order caloricity:', order.calculateCalories());
+
+    order.submit();
+
+    order.addSalad('russian', 500);
+    console.log('Your order price:', order.calculatePrice());
+    console.log('Your order caloricity:', order.calculateCalories());
 
 }
 
